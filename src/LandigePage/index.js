9b50860924a5561baa298deafc5fa75c9dc80bc9
leticiaSouzaDev <lec667121@gmail.com>
2023-12-23 14:08:05 -0300

    adding of  component Header

---
+++ src/LandigePage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from '../components/Header';
+import './style.css'
+
+function LandigePage(){
+    return(
+        <Header/>
+    )
+}
+
+export default LandigePage;
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css'
+
+function Header(){
+    return (
+        <header>
+            <img src="/assets/barbearia-logo.png" alt=""/>
+
+            <img src='/assets/moon.png' alt=''/>
+        </header>
+    )
+}
+
+export default Header
